ba1aee2bb997a9f7e3960c74ce39a662533dfe93
ProjectsbyNate <89910771+ProjectsbyNate@users.noreply.github.com>
2026-09-18 20:12:26 -0700

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,1 +1,10 @@
+import random 
 print("Welcome to the Number Guessing Game!")
+secret_number = random.randint(1, 100)
+guess = int(input("Enter your guess: "))
+if guess == secret_number:
+  print("Congratulations! You guessed the correct number!")
+elif guess < secret_number:
+  print("Your guess is too low!")
+else: 
+  print("Your number is too high!")
